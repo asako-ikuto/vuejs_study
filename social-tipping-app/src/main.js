00263adad4f6a8985da78b90e0ed201cf1561519
ikuto-asako <m131932@gmail.com>
2022-03-01 09:22:18 +0900

    vue課題4-1ユーザ登録実装20220301

--- social-tipping-app/src/main.js
+++ social-tipping-app/src/main.js
@@ -3,7 +3,28 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC3MWe1l1VCMmuny6ly0j6iLhfh2CMh3H8",
+  authDomain: "socialtippingapp.firebaseapp.com",
+  projectId: "socialtippingapp",
+  storageBucket: "socialtippingapp.appspot.com",
+  messagingSenderId: "207712721377",
+  appId: "1:207712721377:web:c26586d527f913a3bd5aa0"
+};
+
+firebase.initializeApp(firebaseConfig)
+
 Vue.config.productionTip = false
+
+const db = firebase.firestore()
+const auth = firebase.auth()
+
+export {db, auth}
 
 new Vue({
   router,
--- social-tipping-app/src/views/SignUp.vue
+++ social-tipping-app/src/views/SignUp.vue
@@ -23,25 +23,7 @@
 </template>
 
 <script>
-//firebase初期化
-import firebase from 'firebase/app'
-import 'firebase/auth'
-import 'firebase/firestore'
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyC3MWe1l1VCMmuny6ly0j6iLhfh2CMh3H8",
-  authDomain: "socialtippingapp.firebaseapp.com",
-  projectId: "socialtippingapp",
-  storageBucket: "socialtippingapp.appspot.com",
-  messagingSenderId: "207712721377",
-  appId: "1:207712721377:web:c26586d527f913a3bd5aa0"
-};
-
-firebase.initializeApp(firebaseConfig)
-
-const db = firebase.firestore()
-const auth = firebase.auth()
+import {db, auth} from '../main.js'
 
 export default {
   name: 'signup',
@@ -56,7 +38,6 @@ export default {
   },
   methods: {
     signUp() {
-
       //ユーザ新規登録
       auth.createUserWithEmailAndPassword(this.email, this.password)
         .then((userCredential) => {
